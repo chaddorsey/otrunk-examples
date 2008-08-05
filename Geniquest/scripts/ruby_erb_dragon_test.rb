ce810f96575c2451c26fd23558e68da8905ae060
Chad Dorsey <chaddorsey@gmail.com>
2008-08-04 21:04:17 -0400

    Add Ruby/Java connections from meiosis view experiment into OTML page

--- Geniquest/scripts/ruby_erb_dragon_test.rb
+++ Geniquest/scripts/ruby_erb_dragon_test.rb
@@ -1,3 +1,5 @@
+require 'java' 
+require 'rbconfig'
 require 'erb'
 include_class 'org.concord.framework.otrunk.OTrunk'
 include_class 'org.concord.biologica.state.OTBreedOffspring'
@@ -9,6 +11,15 @@ include_class 'org.concord.biologica.state.OTSex'
 include_class 'org.concord.otrunk.ui.OTChoiceWithFeedback'
 include_class 'org.concord.otrunk.ui.OTText'
 
+import org.concord.biologica
+import org.concord.biologica.ui
+import org.concord.biologica.engine
+import java.lang
+import java.beans
+include_class org.concord.otrunk.view.OTFolderObject
+include_class org.concord.otrunk.ui.OTText
+include_class org.concord.biologica.ui.UIProp
+include_class org.concord.biologica.engine.EngineProp
 
 def getText
   if $model
@@ -43,4 +54,46 @@ def linkToObject(link_text, obj, view=nil)
   else
     "<a href=\"#{obj.otExternalId}\">#{link_text}</a>"
   end
+end
+
+
+def self.init
+  puts "Initial view"
+  $meiosisViewInternal = $meiosisView.getComponent(0)
+  #$textArea.setCurrentCard("meiosisStr")
+  #$breedOffspringViewInternal = $breedOffspringView.getComponent(0)
+  #$breedOffspringComponent = $breedOffspringView.getComponent(0)
+  #$wingsHeredityChoiceInternal = $wings_heredity_choice.getComponent(0)
+  #puts "Text generated."
+  true
+end
+
+def self.save
+  true
+end
+
+#Listener architecture (non-generic)
+class ClickAction 
+  include java.beans.PropertyChangeListener
+  
+  def propertyChange(evt)
+    if (evt.getPropertyName == UIProp::SEX_VIEW_MODE)
+      case $meiosisViewInternal.getSexViewMode
+      when 1: 
+        puts "Normal view"
+        #$textArea.setCurrentCard("meiosisStr") # Normal view
+        #bean.foo = 'Normal view'
+      when 2 
+        #$textArea.setCurrentCard("motherMagnifiedStr") # Viewing mother chromosomes
+        #bean.foo = 'Mother view'
+        #$meiosisViewInternal.setSexTextVisible(false)
+        puts "Mother view"
+      when 3
+        puts "Father view"
+        #$meiosisViewInternal.setSexTextVisible(true)
+        #$textArea.setCurrentCard("fatherMagnifiedStr") # Viewing father chromosomes
+        #bean.foo = 'Father view'
+      end
+    end
+   end
 end
