0e799b2834f52576848434d567d84e1822be1f64
Chad Dorsey <chaddorsey@gmail.com>
2008-08-21 11:56:25 -0400

    Modify ruby_meiosis_script and ruby_dragon_test_button.otml to make gender text invisible when Sex View Mode is changed.

--- Geniquest/scripts/ruby_meiosis_script.rb
+++ Geniquest/scripts/ruby_meiosis_script.rb
@@ -23,8 +23,18 @@ include_class 'org.concord.otrunk.view.OTFolderObject'
 include_class 'org.concord.biologica.ui.UIProp'
 include_class 'org.concord.biologica.engine.EngineProp'
 
-def self.init
-  puts "self.init called"
+class MeiosisAction
+ include java.beans.PropertyChangeListener
+
+ def propertyChange(evt)
+   if (evt.getPropertyName == UIProp::SEX_VIEW_MODE)
+     case $meiosisViewInternal.getSexViewMode
+     when 1 : $meiosisViewInternal.setSexTextVisible(false) # Normal view
+     when 2 : $meiosisViewInternal.setSexTextVisible(false) # Viewing mother chromosomes
+     when 3 : $meiosisViewInternal.setSexTextVisible(false) # Viewing father chromosomes
+     end
+   end
+ end
 end
 
 def self.clicked
@@ -44,11 +54,13 @@ class LabelRangeResponse
   
   def initialize(response_key)
     @response_key = response_key
+
   end
   
   def clicked
-    $motherOrganism.name = "NewName"
-    $motherView.setNameTextVisible = false
+    #$motherOrganism.name = "NewName"
+    #$motherView.setSexTextVisible(false)
+    $meiosisViewInternal.setSexTextVisible(false)
   end
 
 end
@@ -56,4 +68,12 @@ end
 # displays message in dialog
 def showMessage(message)
   JOptionPane.showMessageDialog(nil, message)
+end
+
+def self.init
+  puts "self.init called"
+  puts "Doing the initialization."
+  $meiosisViewInternal = $meiosisView.getComponent(0)
+  $meiosisViewInternal.addPropertyChangeListener MeiosisAction.new
+  true
 end
