100228f0ad638c1fd16c921e51e3e45c95f2550a
Chad Dorsey <chaddorsey@gmail.com>
2008-08-21 14:26:24 -0400

    Add changes on the way to removing button references.

--- Geniquest/scripts/ruby_meiosis_script.rb
+++ Geniquest/scripts/ruby_meiosis_script.rb
@@ -23,18 +23,8 @@ include_class 'org.concord.otrunk.view.OTFolderObject'
 include_class 'org.concord.biologica.ui.UIProp'
 include_class 'org.concord.biologica.engine.EngineProp'
 
-class MeiosisAction
- include java.beans.PropertyChangeListener
-
- def propertyChange(evt)
-   if (evt.getPropertyName == UIProp::SEX_VIEW_MODE)
-     case $meiosisViewInternal.getSexViewMode
-     when 1 : $meiosisViewInternal.setSexTextVisible(false) # Normal view
-     when 2 : $meiosisViewInternal.setSexTextVisible(false) # Viewing mother chromosomes
-     when 3 : $meiosisViewInternal.setSexTextVisible(false) # Viewing father chromosomes
-     end
-   end
- end
+def self.init
+  puts "self.init called"
 end
 
 def self.clicked
@@ -54,13 +44,11 @@ class LabelRangeResponse
   
   def initialize(response_key)
     @response_key = response_key
-
   end
   
   def clicked
-    #$motherOrganism.name = "NewName"
-    #$motherView.setSexTextVisible(false)
-    $meiosisViewInternal.setSexTextVisible(false)
+    $motherOrganism.name = "NewName"
+    $motherView.setNameTextVisible = false
   end
 
 end
@@ -68,12 +56,4 @@ end
 # displays message in dialog
 def showMessage(message)
   JOptionPane.showMessageDialog(nil, message)
-end
-
-def self.init
-  puts "self.init called"
-  puts "Doing the initialization."
-  $meiosisViewInternal = $meiosisView.getComponent(0)
-  $meiosisViewInternal.addPropertyChangeListener MeiosisAction.new
-  true
 end
