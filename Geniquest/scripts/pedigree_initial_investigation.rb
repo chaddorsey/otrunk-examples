d6349c796703e8aeefcc57d79339c8bc6ecf50d8
Chad Dorsey <chaddorsey@gmail.com>
2008-08-28 00:32:10 -0400

    Other attempts at scripting pedigree view.

---
+++ Geniquest/scripts/pedigree_initial_investigation.rb
@@ -0,0 +1,41 @@
+include_class 'org.concord.biologica.state.OTStaticOrganism'
+include_class 'org.concord.biologica.state.OTPedigree'
+include_class 'org.concord.biologica.state.OTSex'
+include_class 'org.concord.otrunk.ui.OTChoiceWithFeedback'
+include_class 'org.concord.otrunk.ui.OTText'
+
+import org.concord.biologica
+import org.concord.biologica.ui
+import org.concord.biologica.engine
+import java.lang
+import java.beans
+include_class 'org.concord.otrunk.view.OTFolderObject'
+include_class 'org.concord.biologica.ui.UIProp'
+include_class 'org.concord.biologica.engine.EngineProp'
+
+class PedigreeAction
+ include java.beans.PropertyChangeListener
+
+ def propertyChange(evt)
+   if (evt.getPropertyName == UIProp::ORGANISM_ADDED_TO_VIEW)
+     puts "change"
+     $textObject.text = "Great. Now add another generation."
+   end
+ end
+end
+
+# displays message in dialog
+def showMessage(message)
+  JOptionPane.showMessageDialog(nil, message)
+end
+
+def self.init
+  puts "self.init called"
+  $pedigreeViewInternal = $pedigreeView.getComponent(0)
+  $pedigreeViewInternal.addPropertyChangeListener PedigreeAction.new
+  true
+end
+
+def self.save
+  true
+end
