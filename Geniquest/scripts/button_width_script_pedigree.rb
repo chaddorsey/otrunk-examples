28c13f7f22885287055ba865927b91c7e04f6273
Chad Dorsey <chaddorsey@gmail.com>
2008-08-28 10:00:35 -0400

    Update test button script to include pedigree view.

---
+++ Geniquest/scripts/button_width_script_pedigree.rb
@@ -0,0 +1,76 @@
+require 'java' 
+require 'rbconfig'
+require 'erb'
+
+include_class 'javax.swing.JOptionPane'
+
+include_class 'org.concord.framework.otrunk.OTrunk'
+include_class 'org.concord.biologica.state.OTBreedOffspring'
+include_class 'org.concord.biologica.state.OTOrganism'
+include_class 'org.concord.biologica.state.OTWorld'
+include_class 'org.concord.biologica.state.OTChromosome'
+include_class 'org.concord.biologica.state.OTStaticOrganism'
+include_class 'org.concord.biologica.state.OTSex'
+include_class 'org.concord.otrunk.ui.OTChoiceWithFeedback'
+include_class 'org.concord.otrunk.ui.OTText'
+
+import org.concord.biologica
+import org.concord.biologica.ui
+import org.concord.biologica.engine
+import java.lang
+import java.beans
+include_class 'org.concord.otrunk.view.OTFolderObject'
+include_class 'org.concord.biologica.ui.UIProp'
+include_class 'org.concord.biologica.engine.EngineProp'
+
+def self.init
+  puts "self.init called2"
+
+  #$pedigreeViewInternalContext=context.$pedigreeViewComp.getComponent(0)
+  #$context=context.getComponentForObject($pedigreeViewComp).getComponent(0);
+end
+
+def self.clicked
+ response_key = {
+   :width_retrieved => { :text => "The width was retrieved." },
+   :variable_not_set => { :text => "This variable isn't set. Odd, that."},
+   :text_visible => { :text => "The text was visible. I'll make it go away." },
+   :text_invisible => { :text => "The text was invisible. I'll make it appear."}
+ }
+ @label_range_response = LabelRangeResponse.new(response_key)
+ @label_range_response.clicked
+end
+
+class LabelRangeResponse
+  $pedigreeViewInternal=$pedigreeViewComp
+  attr_reader :response_key
+  
+  def initialize(response_key)
+    @response_key = response_key
+  end
+  
+  def clicked
+    #oldwidth=$pedigreeViewVar.getWidth
+    #puts oldwidth
+    #$pedigreeViewVar.setWidth(oldwidth + 5)
+    #momMarkers=$oldMotherObj.getMarkerString
+    #momAlleles = $oldMother.getAlleles
+    #puts momMarkers
+    #puts momAlleles
+    #$oldMotherObj.setVisible(false)
+    numberOfOrganisms=$pedigreeViewInternal.getNumberOfOrganisms
+    puts numberOfOrganisms
+    $pedigreeViewInternal.setOffspringModePulldownVisible(false)
+    $pedigreeViewInternal.setBounds(0,0,500,500)
+    $pedigreeViewInternal.updateSize
+    #newnum=sexViewMode.to_i
+    #newnum == 5? newnum = 1 : newnum = newnum + 1
+    #$pedigreeViewInternal.setSexViewMode(newnum)
+  end
+
+end
+
+# displays message in dialog
+def showMessage(message)
+  JOptionPane.showMessageDialog(nil, message)
+end
---
+++ Geniquest/scripts/ruby_pedigree_script.rb
@@ -0,0 +1,55 @@
+require 'java' 
+require 'rbconfig'
+require 'erb'
+
+include_class 'javax.swing.JOptionPane'
+
+include_class 'org.concord.framework.otrunk.OTrunk'
+include_class 'org.concord.biologica.state.OTBreedOffspring'
+include_class 'org.concord.biologica.state.OTOrganism'
+include_class 'org.concord.biologica.state.OTWorld'
+include_class 'org.concord.biologica.state.OTChromosome'
+include_class 'org.concord.biologica.state.OTStaticOrganism'
+include_class 'org.concord.biologica.state.OTPedigree'
+include_class 'org.concord.otrunk.ui.OTChoiceWithFeedback'
+include_class 'org.concord.otrunk.ui.OTText'
+
+import org.concord.biologica
+import org.concord.biologica.ui
+import org.concord.biologica.engine
+import java.lang
+import java.beans
+include_class 'org.concord.otrunk.view.OTFolderObject'
+include_class 'org.concord.biologica.ui.UIProp'
+include_class 'org.concord.biologica.engine.EngineProp'
+
+class PedigreeAction
+ include java.beans.PropertyChangeListener
+
+ def propertyChange(evt)
+   if (evt.getPropertyName == UIProp::CROSS_SUCCEEDED)
+     puts "Nice cross!"
+     #case $meiosisViewInternal.getSexViewMode
+     #when 1 : $meiosisViewInternal.setSexTextVisible(false) # Normal view
+     #when 2 : $meiosisViewInternal.setSexTextVisible(false) # Viewing mother chromosomes
+     #when 3 : $meiosisViewInternal.setSexTextVisible(false) # Viewing father chromosomes
+     #end
+   end
+ end
+end
+
+# displays message in dialog
+def showMessage(message)
+  JOptionPane.showMessageDialog(nil, message)
+end
+
+def self.init
+  puts "self.init called"
+  $pedigreeViewInternal = $pedigreeView
+  $pedigreeViewInternal.addPropertyChangeListener PedigreeAction.new
+  true
+end
+
+def self.save
+  true
+end
