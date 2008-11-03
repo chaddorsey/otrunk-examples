76c9906cad1f9fe4a4bfcb9acdf484771e6a6eed
Chad Dorsey <chaddorsey@gmail.com>
2008-11-03 09:56:36 -0500

    Modifying multi-peak script to turn off one threshold line

--- Geniquest/scripts/qtl_link_script_all.rb
+++ Geniquest/scripts/qtl_link_script_all.rb
@@ -19,7 +19,7 @@ def self.clicked
   puts "Setting Horizontal visible to false."
   puts $thresh99_99.isHorizontalVisible.to_s
   $thresh99_99.setHorizontalVisible(false)
-  puts $thresh99_99.isHorizontalVisible.to_s  
+  puts $thresh99_99.isHorizontalVisible.to_s
   #puts $QTLSimulation.inspect
   #puts $QTLGraph.inspect
   ##$confidenceIntervalVector = $QTLSimulation.getConfidenceInterval(1.to_s).to_s
@@ -39,11 +39,21 @@ def self.clicked
   
 #    num += 1
 #  end
-
-  puts $QTL_graph_99pct.inspect
-  $QTLGraphables = $QTL_graph_99pct.getGraphables
-  $QTLConfidenceLabels = $QTL_graph_99pct.getLabels.getVector
-  $QTLConfidenceLabels.each {|l| puts l;}
+  puts
+  puts "Inspecting graphs"
+  puts "$QTL_graph_99pct.inspect: " + $QTL_graph_99pct.inspect
+  $QTLGraphLabels = $QTL_graph_99pct.getLabels.getVector
+  puts "99pct Labels: "
+  $QTLGraphLabels.each {|l| puts l;}
+  puts "Setting horizontal visible to false again: "
+  $QTLGraphLabels[0].setHorizontalVisible(false)
+  puts "$QTL_graph_99pct_view.getComponent($QTL_graph_99pct).inspect: " + $QTL_graph_99pct_view.getComponent($QTL_graph_99pct).inspect
+  $QTL_graph_obj_99pct = $QTL_graph_99pct_view.getComponent($QTL_graph_99pct)
+  puts "X scale: " + $QTL_graph_obj_99pct.getXScale.to_s
+  puts "Turning off toolbar."
+  $QTLGraphables = $QTL_graph_obj_99pct.getObjList.getSelectedObjects
+  puts "Object List: "+ $QTLGraphables.inspect
+  puts "Vector: " + $QTLGraphables.size.to_s
   
 
   #$QTLGraph.remove($QTLConfidenceLabels[4])
@@ -51,7 +61,7 @@ def self.clicked
   puts "99pct Graphables: "
   puts
   $QTLGraphableVector.each {|o| puts o.inspect}
-  puts "9pct Graphables DataStores: "
+  puts "99pct Graphables DataStores: "
   puts
   $QTLGraphableVector.each {|o| puts o.getDataStore}
   $QTLDataStoreZero = $QTLGraphableVector[0].getDataStore.getValues
@@ -78,7 +88,7 @@ def self.clicked
     (self * 10**x).round.to_f / 10**x
   end
   
-  $textField.text = "Threshold 99%: " + $thresholdAdjusted[0].round_to(2).to_s + "\nThreshold 90%: " + $thresholdAdjusted[1].round_to(2).to_s + "\nThreshold 85%: " + $thresholdAdjusted[2].round_to(2).to_s
+  #$textField.text = "Threshold 99%: " + $thresholdAdjusted[0].round_to(2).to_s + "\nThreshold 90%: " + $thresholdAdjusted[1].round_to(2).to_s + "\nThreshold 85%: " + $thresholdAdjusted[2].round_to(2).to_s
 end
 def self.init
   puts "self.init called!"
