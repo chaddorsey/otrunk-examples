101aa8a041468b02d7943405692e1d8488314533
Chad Dorsey <chaddorsey@gmail.com>
2008-11-03 13:56:15 -0500

    Example for Randy

--- Geniquest/scripts/qtl_link_script_all.rb
+++ Geniquest/scripts/qtl_link_script_all.rb
@@ -13,59 +13,52 @@ import java.beans
 include_class 'org.concord.otrunk.view.OTFolderObject'
 include_class 'org.concord.biologica.ui.UIProp'
 include_class 'org.concord.biologica.engine.EngineProp'
+#include OTrunkRubyScriptTools
 
 def self.clicked
-  puts $thresh99_99.inspect
-  puts "Setting Horizontal visible to false."
-  puts $thresh99_99.isHorizontalVisible.to_s
-  $thresh99_99.setHorizontalVisible(false)
-  puts $thresh99_99.isHorizontalVisible.to_s
-  #puts $QTLSimulation.inspect
-  #puts $QTLGraph.inspect
-  ##$confidenceIntervalVector = $QTLSimulation.getConfidenceInterval(1.to_s).to_s
-  ##num = 0
-  ##puts $confidenceIntervalVector.size
-  ##puts "confInt: " + $confidenceIntervalVector[0].to_s
-  ##thresh_array=[99,90,35].to_java :double
-  ##$thresholdVector = $QTLSimulation.getThresholdValues(thresh_array)
-  ##puts $thresholdVector.size
-  ##$thresholdAdjusted = $thresholdVector.map{ |v| v+= 5}
-  ##$thresholdAdjusted.each {|v| puts v}
-  ##For some reason, this seems to be 5 lower than the actual LOD threshold output on the graph
-  ##puts "Threshold at 99%: " + $thresholdAdjusted[0].to_s
-  #num = 0
-  #puts $confidenceIntervalVector.size
-  #puts "confInt: " + $confidenceIntervalVector[0].to_s
-  
-#    num += 1
-#  end
-  puts
   puts "Inspecting graphs"
   puts "$QTL_graph_99pct.inspect: " + $QTL_graph_99pct.inspect
   $QTLGraphLabels = $QTL_graph_99pct.getLabels.getVector
   puts "99pct Labels: "
   $QTLGraphLabels.each {|l| puts l;}
   puts "Setting horizontal visible to false again: "
-  $QTLGraphLabels[0].setHorizontalVisible(false)
-  puts "$QTL_graph_99pct_view.getComponent($QTL_graph_99pct).inspect: " + $QTL_graph_99pct_view.getComponent($QTL_graph_99pct).inspect
-  $QTL_graph_obj_99pct = $QTL_graph_99pct_view.getComponent($QTL_graph_99pct)
-  puts "X scale: " + $QTL_graph_obj_99pct.getXScale.to_s
-  puts "Turning off toolbar."
-  $QTLGraphables = $QTL_graph_obj_99pct.getObjList.getSelectedObjects
-  puts "Object List: "+ $QTLGraphables.inspect
-  puts "Vector: " + $QTLGraphables.size.to_s
+  $QTLGraphLabels[0].setHorizontalVisible(false)  
+  $QTLGraphThresh99 = $QTLGraphLabels[0]
+  $QTLGraphThresh90 = $QTLGraphLabels[1]
+  $QTLGraphThresh65 = $QTLGraphLabels[2]
+  $QTLHighlightLabel = $QTLGraphLabels[3]
+  puts "Turning on thresholds." 
   
+  def show_threshold
+    self.setHorizontalVisible(true)
+    self.setLabelVisible(true)
+  end
+  $QTLGraphThresh99.show_threshold
+  
+  puts "Y 99pct threshold: " + $QTLGraphThresh99.yData.to_s
 
-  #$QTLGraph.remove($QTLConfidenceLabels[4])
-  $QTLGraphableVector = $QTLGraphables.getVector
-  puts "99pct Graphables: "
-  puts
-  $QTLGraphableVector.each {|o| puts o.inspect}
-  puts "99pct Graphables DataStores: "
-  puts
-  $QTLGraphableVector.each {|o| puts o.getDataStore}
-  $QTLDataStoreZero = $QTLGraphableVector[0].getDataStore.getValues
-  puts $QTLDataStoreZero.size
+  puts "99pct confidence interval from " + ($QTLHighlightLabel.x1 * 10000).to_i.to_s  + " to " + ($QTLHighlightLabel.x2 * 10000).to_i.to_s
+  
+  
+  #puts "$QTL_graph_99pct_view.getComponent($QTL_graph_99pct).inspect: " + $QTL_graph_99pct_view.getComponent($QTL_graph_99pct).inspect
+  #$QTL_graph_obj_99pct = $QTL_graph_99pct_view.getComponent($QTL_graph_99pct)
+  #puts "X scale: " + $QTL_graph_obj_99pct.getXScale.to_s
+  #puts "Turning off toolbar."
+  #$QTLGraphables = $QTL_graph_obj_99pct.getObjList.getSelectedObjects
+  #puts "Object List: "+ $QTLGraphables.inspect
+  #puts "Vector: " + $QTLGraphables.size.to_s
+  #
+  #
+  ##$QTLGraph.remove($QTLConfidenceLabels[4])
+  #$QTLGraphableVector = $QTLGraphables.getVector
+  #puts "99pct Graphables: "
+  #puts
+  #$QTLGraphableVector.each {|o| puts o.inspect}
+  #puts "99pct Graphables DataStores: "
+  #puts
+  #$QTLGraphableVector.each {|o| puts o.getDataStore}
+  #$QTLDataStoreZero = $QTLGraphableVector[0].getDataStore.getValues
+  #puts $QTLDataStoreZero.size
 
   #num = 0
   #while num < $QTLDataStoreZero.size
@@ -87,7 +80,9 @@ def self.clicked
   def round_to(x)
     (self * 10**x).round.to_f / 10**x
   end
-  
+  def to_kbp
+    self * 10000
+  end
   #$textField.text = "Threshold 99%: " + $thresholdAdjusted[0].round_to(2).to_s + "\nThreshold 90%: " + $thresholdAdjusted[1].round_to(2).to_s + "\nThreshold 85%: " + $thresholdAdjusted[2].round_to(2).to_s
 end
 def self.init
