8c6c3075c66401a7eafa3233af99f39457447e30
Chad Dorsey <chaddorsey@gmail.com>
2008-11-07 05:18:24 -0500

    Checking in version of working script for pilot experiment runs with budget.

--- Geniquest/scripts/qtl_link_script_commongraph.rb
+++ Geniquest/scripts/qtl_link_script_commongraph.rb
@@ -15,17 +15,29 @@ include_class 'org.concord.biologica.ui.UIProp'
 include_class 'org.concord.biologica.engine.EngineProp'
 #include OTrunkRubyScriptTools
 
-@remaining_budget = 500000
 
 def self.clicked
   puts "Inspecting graphs"
-  puts "Initial budget: " + @remaining_budget.to_s
+  puts "Initial budget: " + $remainingBudget.value.to_s
   puts "$QTL_graph.inspect!!: " + $QTLGraph.inspect
   $QTLGraphables = $QTLGraph.getGraphables.getVector
+  $QTLLabels = $QTLGraph.getLabels.getVector
+  #$QTLLabels.each {|l| puts l.inspect}
+
+##Turn off all labels
+  for n in 0..17 do
+   $QTLLabels[n].setHorizontalVisible(false)
+   $QTLLabels[n].setVerticalVisible(false)
+  end
+
   puts "Graphables: " + $QTLGraphables.inspect
   $QTLGraphables.each {|g| g.setLineWidth(2.0)}
   #$QTLGraphables.each {|g| g.setColor(0x00FF0000)}
+
+  #Turn off all graphables
+  
   $QTLGraphables.each {|g| g.setVisible(false)}
+
   #$QTLGraphables[0].inspect
   #$QTLGraphables[0].setVisible(false)
   puts "$drakesCheckBoxes.inspect: " + $drakesCheckBoxes.inspect
@@ -33,6 +45,8 @@ def self.clicked
   puts "$drakesCheckBoxes.getCurrentChoices: "
   puts $selectedDrakes.inspect
   puts $selectedDrakes.size > 2
+
+  #Get the names of the selected Drake strains
   n=0
   $selectedDrakeStrains = Array.new
   $selectedDrakes.each do |drake|
@@ -47,29 +61,53 @@ def self.clicked
     
   end
   
+  #Iterate budget down a notch and turn on corresponding graphs and labels
+  
   if ($selectedDrakes.size == 2)
-    @remaining_budget += -100000
-    puts @remaining_budget
-    $textField.text = "Budget Remaining: $" + @remaining_budget.to_s
-    if (@remaining_budget != 0)
+    $remainingBudget.value += -100000
+    puts $remainingBudget.value.to_s
+    $budgetText.text = "Budget Remaining: $" + $remainingBudget.value.to_s
+    if ($remainingBudget.value > 0)
       if $selectedDrakeStrains.include?("Mountain")
         puts "Mountain"
         $QTLGraphables[0].setVisible(true)
+        $QTLLabels[0].setHorizontalVisible(true)
+        $QTLLabels[1].setHorizontalVisible(true)
+        $QTLLabels[2].setHorizontalVisible(true)
+        $QTLLabels[3].setVerticalVisible(true)
+        $QTLLabels[4].setVerticalVisible(true)
         $QTLGraphables[0].setColor(0x00FF0000)
       elsif $selectedDrakeStrains.include?("Valley")
         puts "Valley"
         $QTLGraphables[1].setVisible(true)
         $QTLGraphables[1].setColor(0x00FF0000)
+        $QTLLabels[5].setHorizontalVisible(true)
+        $QTLLabels[6].setHorizontalVisible(true)
+        $QTLLabels[7].setHorizontalVisible(true)
+        $QTLLabels[8].setVerticalVisible(true)
+        $QTLLabels[9].setVerticalVisible(true)
       elsif $selectedDrakeStrains.include?("Swamp")
         puts "Swamp"
         $QTLGraphables[2].setVisible(true)
         $QTLGraphables[2].setColor(0x00FF0000)
+        $QTLLabels[10].setHorizontalVisible(true)
+        $QTLLabels[11].setHorizontalVisible(true)
+        $QTLLabels[12].setHorizontalVisible(true)
+        $QTLLabels[13].setVerticalVisible(true)
+        $QTLLabels[14].setVerticalVisible(true)
       elsif $selectedDrakeStrains.include?("Desert" || "Ice" || "Forest") 
         puts "Other"
         $QTLGraphables[3].setColor(0x00FF0000)
         $QTLGraphables[3].setVisible(true)
+        $QTLLabels[15].setHorizontalVisible(true)
+        $QTLLabels[16].setHorizontalVisible(true)
+        $QTLLabels[17].setHorizontalVisible(true)
+        $QTLLabels[18].setVerticalVisible(true)
+        $QTLLabels[19].setVerticalVisible(true)
       end
-    else $textField.text = "You have no money remaining!"
+    else
+      $remainingBudget.value = 0 
+      $budgetText.text = "You have no money remaining! Proceed to the next page to use the results of your experimental runs to look for genetic clues to the disease."
     end
   else $textField.text = "Please select two strains of Drakes for this run." 
   end
